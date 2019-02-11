392518aa3bff770183d1e66ca1ee4398245f5550
Claude Nai <cniinai@gmail.com>
2019-02-11 11:55:40 +0000

    mod 5 assignment refactoring

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -105,7 +105,7 @@ function buildAndShowHomeHTML (categories) {
       // var chosenCategoryShortName = ....
       chooseRandomCategory(categories)
 
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      var chosenCategoryShortName = "'"+chooseRandomCategory(categories).short_name+"'";
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -119,8 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
-      chosenCategoryShortName = "'"+chosenCategoryShortName+"'";
+      
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', chosenCategoryShortName)
 
 
